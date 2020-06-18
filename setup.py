aae6a9250d094b86e74124130d69e82978e56503
dharvey-consbio <daniel.harvey@consbio.org>
2020-06-18 11:40:13 -0700

    Add setup file for TWSAndExplore

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='EEMS',
+    version='2.0.3',
+    description='Environmental Evaluation Modeling System',
+    url='https://github.com/consbio/EEMS',
+    author='Tim Sheehan, M.S.',
+    author_email='tim@consbio.org',
+    license='BSD',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Logic Modeling :: Tools',
+        'Programming Language :: Python :: 3.*'
+    ],
+    keywords='environment,modeling,eems',
+    packages=[''],
+    install_requires=['numpy'],
+)
