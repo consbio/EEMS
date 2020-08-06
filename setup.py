d4db6550fc364167a4368e0a36c33e46db3330d7
dharvey-consbio <daniel.harvey@consbio.org>
2020-08-06 12:17:56 -0700

    Increment version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='EEMS',
-    version='2.0.3',
+    version='2.0.4',
     description='Environmental Evaluation Modeling System',
     url='https://github.com/consbio/EEMS',
     author='Tim Sheehan, M.S.',
